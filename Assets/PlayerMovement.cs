8fab42be20ef0ccb3830419894015adba9c910fe
Mirabell <elmirabegieva36@gmail.com>
2024-12-05 12:31:52 +0300

    Added rotation via Quaternion

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,9 @@ public class PlayerMovement : MonoBehaviour
     public float forwardspeed =10f;
     public float speed = 10f;
     private Rigidbody rb;
+    
+    public float rotationSpeed = 100f; // rotation
+    public float maxTiltAngle = 30f;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -16,5 +19,11 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
 
         rb.velocity =  new Vector3(horizontalInput * speed, 0,0);
+
+        float tiltAngle = -horizontalInput * maxTiltAngle;
+        
+        Quaternion wingTilt = Quaternion.Euler(0, 0, tiltAngle);
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, wingTilt, Time.fixedDeltaTime * 5f);
     }
 }
