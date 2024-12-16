22a5ef7d0ac01fbc9bf22a4630204a1ed77a07f1
Mirabell <elmirabegieva36@gmail.com>
2024-12-16 22:46:10 +0300

    Added damge effect

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,19 +1,27 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     public Image damageEffect;
     public float damageAlpha = 0.25f;
+    public float damageFadeSpeed = 0.4f;
 
     public static UIController instance;
     private void Awake()
     {
         instance = this;
     }
-    
+    void Update()
+    {
+        if (damageEffect.color.a != 0)
+        {
+            damageEffect.color = new Color(damageEffect.color.r, damageEffect.color.g, damageEffect.color.b, 
+                Mathf.MoveTowards(damageEffect.color.a, 0f, damageFadeSpeed * Time.deltaTime));
+        }
+    }
     public void ShowDamage()
     {
-        damageEffect.color = new Color(damageEffect.color.r, damageEffect.color.g, damageEffect.color.b, damageAlpha);
+        damageEffect.color = new Color(damageEffect.color.r, damageEffect.color.g, damageEffect.color.b, 
+            damageAlpha);
     }
 }
