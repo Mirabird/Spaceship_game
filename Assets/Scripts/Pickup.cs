d4ed0cbe69425a00429360d55ea5b85c652d62b2
Mirabell <elmirabegieva36@gmail.com>
2024-12-17 15:14:00 +0300

    Added scoring system

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 public class Pickup : MonoBehaviour
 {
     public int coins = 0; 
     public GameObject pickupEffect;
+    public Text scoreText;
+    void Start()
+    {
+        scoreText.text = "SCORE: 0";
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Coin")
         {
             Instantiate(pickupEffect, transform.position, transform.rotation);
             coins++;
+            scoreText.text = "SCORE:" + coins;
             Destroy(other.gameObject);
         }
     }
