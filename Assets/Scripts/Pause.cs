9571ccb7980d854a79a924c957e38272c790a566
Mirabell <elmirabegieva36@gmail.com>
2024-12-17 23:54:24 +0300

    Added Pause mechanics

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    public bool isPaused;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseUnpause();
+        }
+    }
+    public void PauseUnpause()
+    {
+        if (isPaused)
+        {
+            isPaused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1f;
+            AudioListener.pause = false;
+        }
+        else
+        {
+            isPaused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0f;
+            AudioListener.pause = true; 
+        }
+    }
+    public void MenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+        AudioListener.pause = false;  
+    }
+}
