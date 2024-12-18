6190413e9420df5f7a553fe1bf5d430b8643b5bb
Mirabell <elmirabegieva36@gmail.com>
2024-12-18 13:26:43 +0300

    Added Display score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public GameObject gameOverscreen;
+    
+    private Pickup _pickupScript;
+    public Text gameOverScoreText;
     private void Awake()
     {
         if (instance == null)
@@ -14,9 +18,36 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    void Start()
+    {
+        _pickupScript = FindObjectOfType<Pickup>(); 
+        
+        if (_pickupScript == null)
+        {
+            Debug.LogError("Pickup script not found in the scene!");
+        }
+        
+        if (gameOverscreen == null)
+        {
+            Debug.LogError("GameOverScreen is not assigned in the inspector!");
+        }
+
+        if (gameOverScoreText == null)
+        {
+            Debug.LogError("GameOverScoreText is not assigned in the inspector!");
+        }
+    }
     public void ShowGameOver()
     {
-        gameOverscreen.SetActive(true);
-        Time.timeScale = 0f;
+        if (_pickupScript != null)
+        {
+            gameOverscreen.SetActive(true);
+            gameOverScoreText.text = "SCORE: " + _pickupScript.coins;
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Debug.LogError("Cannot show game over screen because Pickup is null!");
+        }
     }
 }
