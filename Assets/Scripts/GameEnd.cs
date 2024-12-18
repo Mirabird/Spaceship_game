620dc839a872d988a182c13c74cebba78303202c
Mirabell <elmirabegieva36@gmail.com>
2024-12-18 16:25:31 +0300

    Added Game Ending Window

---
+++ Assets/Scripts/GameEnd.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameEnd : MonoBehaviour
+{
+    public void MenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+ 
+}
